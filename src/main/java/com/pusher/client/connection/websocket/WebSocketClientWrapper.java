795d232bd2603ede3661cfc1a130e29b26e3984b
Phil Leggetter <phil@leggetter.co.uk>
2013-08-07 23:12:28 +0100

    formatting

--- src/main/java/com/pusher/client/connection/websocket/WebSocketClientWrapper.java
+++ src/main/java/com/pusher/client/connection/websocket/WebSocketClientWrapper.java
@@ -12,55 +12,54 @@ import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.handshake.ServerHandshake;
 
 /**
- * A thin wrapper around the WebSocketClient class from the Java-WebSocket library. The purpose of this
- * class is to enable the WebSocketConnection class to be unit tested by swapping out an instance of
- * this wrapper for a mock version. 
+ * A thin wrapper around the WebSocketClient class from the Java-WebSocket
+ * library. The purpose of this class is to enable the WebSocketConnection class
+ * to be unit tested by swapping out an instance of this wrapper for a mock
+ * version.
  */
 public class WebSocketClientWrapper extends WebSocketClient {
 
 	private static final String WSS_SCHEME = "wss";
 	private final WebSocketListener proxy;
-    
+
 	public WebSocketClientWrapper(URI uri, WebSocketListener proxy)
 			throws SSLException {
 		super(uri);
 
-		if (uri.getScheme().equals( WSS_SCHEME )) {
+		if (uri.getScheme().equals(WSS_SCHEME)) {
 			try {
 				SSLContext sslContext = SSLContext.getInstance("TLS");
 				sslContext.init(null, null, null);
 
 				this.setWebSocketFactory(new DefaultSSLWebSocketClientFactory(
 						sslContext));
-			}
-			catch (NoSuchAlgorithmException e) {
+			} catch (NoSuchAlgorithmException e) {
 				throw new SSLException(e);
-			}
-			catch (KeyManagementException e) {
+			} catch (KeyManagementException e) {
 				throw new SSLException(e);
 			}
 		}
 
 		this.proxy = proxy;
 	}
-    
-    @Override
-    public void onOpen(ServerHandshake handshakedata) {
-	proxy.onOpen(handshakedata);
-    }
 
-    @Override
-    public void onMessage(String message) {
-	proxy.onMessage(message);
-    }
+	@Override
+	public void onOpen(ServerHandshake handshakedata) {
+		proxy.onOpen(handshakedata);
+	}
 
-    @Override
-    public void onClose(int code, String reason, boolean remote) {
-	proxy.onClose(code, reason, remote);
-    }
+	@Override
+	public void onMessage(String message) {
+		proxy.onMessage(message);
+	}
 
-    @Override
-    public void onError(Exception ex) {
-	proxy.onError(ex);
-    }    
+	@Override
+	public void onClose(int code, String reason, boolean remote) {
+		proxy.onClose(code, reason, remote);
+	}
+
+	@Override
+	public void onError(Exception ex) {
+		proxy.onError(ex);
+	}
 }
